36905151dc5ce406655cfb1c937258aaedc0f99c
bwas-bellin <benjamin.wasilewski@bellin.org>
2023-12-01 15:16:44 -0600

    1 day ago, 5 days ago

--- TimeAgo.test.js
+++ TimeAgo.test.js
@@ -1,6 +1,6 @@
 const TimeAgo = require('./TimeAgo')
 
-const validDatestring = '2023-12-01T18:00:02.000Z'
+const validDatestring = '2023-11-01T18:00:02.000Z'
 const invalidDatestring = 'lorem-ipsum'
 const validDate = new Date(validDatestring)
 
@@ -10,4 +10,16 @@ test('returns a string', () => {
 
 test('returns an empty string if the input is NaN', () => {
     expect(TimeAgo(invalidDatestring)).toBe('')
+})
+
+test('returns "1 day ago"', () => {
+    const oneDayAgo = new Date()
+    oneDayAgo.setDate(oneDayAgo.getDate() - 1)
+    expect(TimeAgo(oneDayAgo.toISOString())).toBe('1 day ago')
+})
+
+test('returns "5 days ago"', () => {
+    const fiveDaysAgo = new Date()
+    fiveDaysAgo.setDate(fiveDaysAgo.getDate() - 5)
+    expect(TimeAgo(fiveDaysAgo.toISOString())).toBe('5 days ago')
 })
